21fe4f76694df5d45d31bb75fce7f3f9c568a8e5
Shelan Perera <shelanrc@gmail.com>
2016-01-20 15:05:40 +0100

    Update testFlink.rb

--- recipes/testFlink.rb
+++ recipes/testFlink.rb
@@ -1,6 +1,6 @@
 bash 'run_flink_test' do
   cwd "/tmp/streaming-benchmarks"
-  owner node[:streamingExperiment][:user]
+  user node[:streamingExperiment][:user]
   group node[:streamingExperiment][:group]
   code <<-EOM
     ./stream-bench.sh FLINK_TEST
