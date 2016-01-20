5224a3f560bf75f7cea62a2ceb3ec97ce2e6f09a
Shelan Perera <shelanrc@gmail.com>
2016-01-20 16:47:41 +0100

    Update setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,7 +2,6 @@
 bash 'setup' do
   user node[:streamingExperiment][:user]
   group node[:streamingExperiment][:group]
-  cwd "/tmp/streaming-benchmarks"
   code <<-EOM
      ./stream-bench.sh SETUP
   EOM
